7930e1a5dec06d0baa144aa1af0f2ccb6a4cbc38
mBlum96 <m.blum.1996@gmail.com>
2024-11-02 14:58:48 -0600

    can select and upload receipts to the app

---
+++ compareMarket/src/hooks/useImagePicker.ts
@@ -0,0 +1,36 @@
+import { Alert } from 'react-native';
+import {Asset, ImagePickerResponse, launchCamera, launchImageLibrary} from 'react-native-image-picker';
+import {useState} from 'react';
+import { set } from 'mongoose';
+
+const useImagePicker = () => {
+
+    const options = {
+        mediaType: 'photo' as const,
+        selectionLimit: 1,
+    };
+
+    const [images, setImages] = useState<Asset[]>([]);
+    const [error, setError] = useState<string | null>(null);
+
+    const openImagePicker = () => {
+        launchImageLibrary(options, (response: ImagePickerResponse) => {
+            if(response.didCancel){
+                console.log('User cancelled image picker');
+            } else if(response.errorCode){
+                console.error(response.errorMessage);
+                setError(response.errorMessage || 'An error occurred');
+                Alert.alert('Error', response.errorMessage);
+            }
+            else if(response.assets && response.assets.length>0){
+                const selectedImage = response.assets[0];
+                setImages([selectedImage]);
+            }
+        });
+    };
+
+    return {images,error, openImagePicker};
+};
+
+
+export default useImagePicker;
